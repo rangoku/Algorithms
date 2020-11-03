40fd896f4fca1465a47d6e8af067fdfd6a1cec0e
Eugene <Zhenya_Oleynik@users.noreply.github.com>
2020-11-03 13:27:11 +0200

    Main updated

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,6 @@
 package main;
 
-import algorithms.graphs.*;
-import algorithms.sorts.BubbleSort;
-import algorithms.sorts.CombSort;
-import algorithms.sorts.QuickSort;
+import algorithms.sorts.*;
 
 import java.util.Arrays;
 
@@ -11,21 +8,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Integer[] a1 = {12,3,4,100,5,6,5,4,3,3,4,4,5,5,6,7,7,78,7,6,5,3,23,2,3};
-        Integer[] a2 = {12,3,4,100,5,6,5,4,3,3,4,4,5,5,6,7,7,78,7,6,5,3,23,2,3};
+        Integer[] a1 = {12,3,4,100,5,6,5,4,3,3,4,4,5,5,6,7,7,78,7,6,5,3,23,2,3,23,13324,42,1,3,42,2,36,27,81};
+        Integer[] a2 = {12,3,4,100,5,6,5,4,3,3,4,4,5,5,6,7,7,78,7,6,5,3,23,2,3,23,13324,42,1,3,42,2,36,27,81};
+
 
         System.out.println("Original array:");
         System.out.println(Arrays.toString(a1));
 
-        long durationOfCombSort = Profiler.profile(() -> CombSort.sort(a1));
-        long durationOfQSort = Profiler.profile(() -> QuickSort.sort(a2));
+        System.out.println("Selection sort : " +
+                Profiler.profile(() -> SelectionSort.sort(a1))
+                + "ms");
 
-        System.out.println("Sorted array:");
-        System.out.println(Arrays.toString(a1));
-        System.out.println(Arrays.toString(a2));
+        System.out.println("Heap sort : " +
+                Profiler.profile(() -> HeapSort.sort(a2))
+                + "ms");
 
-        System.out.println("CombSort: " + durationOfCombSort + "ms");
-        System.out.println("QSort: " + durationOfQSort + "ms");
 
+        System.out.println("a1: " + Arrays.toString(a1));
+        System.out.println("a2: " + Arrays.toString(a2));
     }
 }
